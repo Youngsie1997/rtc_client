c2a56caf6e8d81bbcde2e92a54f7672a99d62614
Connor Youngs <connormyoungs@gmail.com>
2023-10-06 15:52:57 +0100

    first working version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Peer } from "peerjs";
+  var friendId = "andyfreeborough";
+  var peer;
+  var message = "hello world";
+  var conn;
+
+  function connect() {
+    peer = new Peer("pineapple-sunglasses", { debug: 3 });
+    peer.on("connection", function (conn) {
+      conn.on("data", function (data) {
+        console.log(data);
+      });
+    });
+
+    conn = peer.connect(friendId);
+    // on open will be launch when you successfully connect to PeerServer
+    conn.on("open", function () {
+      console.log("sending message");
+      conn.send("Made contact");
+      console.log(conn.id);
+    });
+  }
+
+  function sendMessage() {
+    conn = peer.connect(friendId);
+    conn.on("open", function () {
+      conn.send(message);
+    });
+  }
+</script>
+
+<h1>WebRtc Client</h1>
+
+<button on:click={sendMessage}>Send Message</button>
+<button on:click={connect}>Connect</button>
+<br />
+
+<label for="messageBox">Message</label>
+<input type="text" id="messageBox" bind:value={message} />
+<br />
+<label for="friendID">Friend</label>
+<input type="text" bind:value={friendId} />
---
+++ src/routes/friend/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Peer } from "peerjs";
+  const friendId = "pineapple-sunglasses";
+  const peer = new Peer("pineapple-testicles");
+
+  function sendMessage() {
+    peer.on("connection", function (conn) {
+      conn.on("data", function (data) {
+        console.log(data);
+      });
+    });
+
+    var conn = peer.connect(friendId);
+    // on open will be launch when you successfully connect to PeerServer
+    conn.on("open", function () {
+      console.log("sending message");
+      conn.send("Hello earthlings!");
+      console.log(conn.id);
+    });
+  }
+</script>
+
+<h1>WebRtc Client</h1>
+
+<button on:click={sendMessage}>Deploy Missle!</button>
