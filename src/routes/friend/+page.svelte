cdf34860690d283151dd68356d7ba31912b379db
Connor Youngs <connormyoungs@gmail.com>
2023-10-06 16:20:46 +0100

    Changed friend route to use the upgraded client

--- src/routes/friend/+page.svelte
+++ src/routes/friend/+page.svelte
@@ -1,25 +1,43 @@
 <script>
   import { Peer } from "peerjs";
-  const friendId = "pineapple-sunglasses";
-  const peer = new Peer("pineapple-testicles");
+  var friendId = "pineapple-sunglasses";
+  var peer;
+  var message = "hello world";
+  var conn;
 
-  function sendMessage() {
+  function connect() {
+    peer = new Peer("pineapple-testicles", { debug: 3 });
     peer.on("connection", function (conn) {
       conn.on("data", function (data) {
         console.log(data);
       });
     });
 
-    var conn = peer.connect(friendId);
+    conn = peer.connect(friendId);
     // on open will be launch when you successfully connect to PeerServer
     conn.on("open", function () {
       console.log("sending message");
-      conn.send("Hello earthlings!");
+      conn.send("Made contact");
       console.log(conn.id);
+    });
+  }
+
+  function sendMessage() {
+    conn = peer.connect(friendId);
+    conn.on("open", function () {
+      conn.send(message);
     });
   }
 </script>
 
 <h1>WebRtc Client</h1>
 
-<button on:click={sendMessage}>Deploy Missle!</button>
+<button on:click={sendMessage}>Send Message</button>
+<button on:click={connect}>Connect</button>
+<br />
+
+<label for="messageBox">Message</label>
+<input type="text" id="messageBox" bind:value={message} />
+<br />
+<label for="friendID">Friend</label>
+<input type="text" bind:value={friendId} />
